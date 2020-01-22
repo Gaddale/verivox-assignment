140e419fae5237afe4fc1f4af142d2e21d8c68b8
Nagaraj Rao G <nagaraja.gaddale@gmail.com>
2020-01-22 23:44:28 +0530

    Update CommonUtils.java

--- src/main/java/com/verivox/assignment/utils/CommonUtils.java
+++ src/main/java/com/verivox/assignment/utils/CommonUtils.java
@@ -5,18 +5,6 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 public class CommonUtils {
 
-    public static String createRamdomString() {
-        return RandomStringUtils.randomAlphanumeric(12);
-    }
-
-    public static String createRandomFirstLastName() {
-        return RandomStringUtils.random(10, true, false);
-    }
-
-    public static Integer createRandomNumber() {
-        return Integer.valueOf(RandomStringUtils.randomNumeric(1));
-    }
-
     public static String setBrowser() {
         String browser = System.getProperty("browser");
         if (browser == null) {
