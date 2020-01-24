bd3d991e92ef32bd94736b8af5d4346828121424
Nagaraj Rao G <nagaraja.gaddale@gmail.com>
2020-01-24 15:52:41 +0530

    Update TariffPage.java

--- src/main/java/com/verivox/assignment/pages/TariffPage.java
+++ src/main/java/com/verivox/assignment/pages/TariffPage.java
@@ -8,14 +8,19 @@ import java.util.List;
 
 public class TariffPage extends BasePage {
 
+    private String searchResultsSting = "[class=\"row my-4\"]>div>app-tariff";
     private By mehrZumTarifButton = By.xpath("(//div[@class=\"row my-4\"]/descendant::app-tariff-cta)[1]");
-    private By searchResults = By.cssSelector("[class=\"row my-4\"]>div>app-tariff");
+    private By searchResults = By.cssSelector(searchResultsSting);
     private By tariffPrice = By.xpath("(//div[@class=\"row my-4\"]/descendant::app-tariff-price/div/div[@class=\"position-relative large mb-2\"]/strong)[1]");
     private By weitereTarifeLadenButton = By.xpath("//button[contains(text(),'weitere tarife laden')]");
-    private String searchResultsSting = "[class=\"row my-4\"]>div>app-tariff";
+    private By loader = By.cssSelector("[class=\"vx-spinner\"]");
     
     public TariffPage(WebDriver driver) {
         super(driver);
+    }
+
+    public WebElement getLoader(){
+        return getElement(loader);
     }
 
     public WebElement getSearchResults() {
