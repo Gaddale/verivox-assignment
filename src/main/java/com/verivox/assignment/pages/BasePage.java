2ad58e81843e1ae57fcdb37deb08ece595b53006
Nagaraj Rao G <nagaraja.gaddale@gmail.com>
2020-01-24 15:50:46 +0530

    Update BasePage.java

--- src/main/java/com/verivox/assignment/pages/BasePage.java
+++ src/main/java/com/verivox/assignment/pages/BasePage.java
@@ -1,5 +1,6 @@
 package com.verivox.assignment.pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -10,7 +11,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
+import static org.apache.log4j.Logger.getLogger;
+
 public abstract class BasePage {
+
+    private static Logger logger = getLogger(BasePage.class.getName());
 
     private WebDriver driver;
 
@@ -38,33 +43,37 @@ public abstract class BasePage {
     public List<WebElement> elementsList(By locator) {
         return driver.findElements(locator);
     }
-    
+
     public void visibilityOfElementLocatedCss(String css) {
         new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(css)));
+    }
+
+    public void visibilityOfElementLocatedXpath(String xpath) {
+        new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
     }
 
     protected WebElement getXpath(By locator) {
         return driver.findElement(locator);
     }
 
-    public void scrollDown(){
+    public void scrollDown() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,700)");
 
     }
 
-    public void scrollIntoView(WebElement element){
+    public void scrollIntoView(WebElement element) {
         ((JavascriptExecutor) driver)
                 .executeScript("arguments[0].scrollIntoView(true); window.scrollBy(0,-100);", element);
     }
-    
+
     public void waitForElementInvisibility(WebElement ele) {
-    try {
-      WebDriverWait wait = new WebDriverWait(driver, 20);
-      wait.until(ExpectedConditions.invisibilityOf(ele));
-    } catch (Exception e) {
-      logger.info("Element Invisiblity Time out");
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 20);
+            wait.until(ExpectedConditions.invisibilityOf(ele));
+        } catch (Exception e) {
+            logger.info("Element Invisiblity Time out");
+        }
     }
-  }
 
 }
